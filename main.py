d7e9021155eee2b1030a8bce80d6471b958fd0fb
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-11 17:30:58 -0500

    managed to add a bingo paper

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+my2d_list=[[random.randint(0,90),random.randint(0,90),random.randint(0,90)],
+           [random.randint(0,90),'Bingo',random.randint(0,90)],
+           [random.randint(0,90),random.randint(0,90),random.randint(0,90)]]
+print(my2d_list)
