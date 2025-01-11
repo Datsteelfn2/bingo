4453d669c83f8ffe1bea4ff79dcac7a4436376dd
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-11 17:47:30 -0500

    printed based on each row

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import random
 my2d_list=[[random.randint(0,90),random.randint(0,90),random.randint(0,90)],
            [random.randint(0,90),'Bingo',random.randint(0,90)],
            [random.randint(0,90),random.randint(0,90),random.randint(0,90)]]
-print(my2d_list)
+
+def rows():
+    for row in my2d_list:
+        print(row)
+rows()
