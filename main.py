5d1861acfad724c06f03b97c524bc5543d4054bf
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-13 23:01:12 -0500

    User can now guess a number in the card and if that  number is picked it gets marked as a X

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import random
 my2d_list=[[random.randint(0,90),random.randint(0,90),random.randint(0,90)],
            [random.randint(0,90),'Bingo',random.randint(0,90)],
            [random.randint(0,90),random.randint(0,90),random.randint(0,90)]]
-
-def rows():
+def card():
     for row in my2d_list:
         print(row)
-rows()
+card()
+while True:
+    guess=int(input("Guess a number:> "))
+    for row in range(len(my2d_list)):
+        for item in range(len(my2d_list[row])):
+            if my2d_list[row][item]==guess:
+                my2d_list[row][item]="X"
+                card()
+    exit=input("Would you like to exit the program:> ")
+    if exit.strip().lower()[0]=="y":
+        break
+    
+print("Youve exited the program")
